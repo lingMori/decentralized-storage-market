b012eef6016945ff6f8ec3012975e751729db95c
lingMori <754597673@qq.com>
2024-11-14 23:21:52 +0800

    (fix): fix old types

---
+++ frontend/src/lib/contract-interact/graphSQL/temp/findFilesbyAddr.ts
@@ -0,0 +1,21 @@
+import { gql, request } from "graphql-request";
+
+export const findFilesbyAddr = (address: string):string => {
+  const subAddress = address.toLowerCase();
+  const graphSQL = gql `{
+    files(where: {owner: "${subAddress}"}) {
+      id
+      cid
+      owner {
+        id
+      }
+      size
+      fileName
+      fileType
+      isActive
+      lastUpdated
+      createdAt
+    }
+  }`;
+  return graphSQL;
+}
